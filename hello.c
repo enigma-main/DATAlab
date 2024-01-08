e09ed104aae978b26aa31577b51d7eb5bc80492e
SH <ksh35@dgist.ac.kr>
2024-01-08 14:08:29 +0900

    Add simple hello.c

---
+++ hello.c
@@ -0,0 +1,133 @@
+/*
+  FUSE: Filesystem in Userspace
+  Copyright (C) 2001-2007  Miklos Szeredi <miklos@szeredi.hu>
+
+  This program can be distributed under the terms of the GNU GPLv2.
+  See the file COPYING.
+*/
+
+/** @file
+ *
+ * minimal example filesystem using high-level API
+ *
+ * Compile with:
+ *
+ *     gcc -Wall hello.c `pkg-config fuse3 --cflags --libs` -o hello -ug
+ *
+ * ## Source code ##
+ * \include hello.c
+ */
+
+// 0804 : readdir, getattr, init is okay, we can do cd otfs, ls
+// mkdir says "There is no file" and makes Input/Output Error
+
+#define FUSE_USE_VERSION 31
+
+#include <fuse.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <assert.h>
+#include <sys/stat.h>
+//#include "bitmap.h"
+
+/*
+ * Command line options
+ *
+ * We can't set default values for the char* fields here because
+ * fuse_opt_parse would attempt to free() them when the user specifies
+ * different values on the command line.
+ */
+static struct options {
+	const char *filename;
+	const char *contents;
+	int show_help;
+} options;
+
+#define OPTION(t, p)                           \
+    { t, offsetof(struct options, p), 1 }
+
+static const struct fuse_opt option_spec[] = {
+	OPTION("--name=%s", filename),
+	OPTION("--contents=%s", contents),
+	OPTION("-h", show_help),
+	OPTION("--help", show_help),
+	FUSE_OPT_END
+};
+
+
+int _g_fd;
+
+static void show_help(const char *progname)
+{
+	printf("usage: %s [options] <mountpoint>\n\n", progname);
+	printf("File-system specific options:\n"
+	       "    --name=<s>          Name of the \"hello\" file\n"
+	       "                        (default: \"hello\")\n"
+	       "    --contents=<s>      Contents \"hello\" file\n"
+	       "                        (default \"Hello, World!\\n\")\n"
+	       "\n");
+}
+
+static void *ot_init(struct fuse_conn_info *conn,
+			struct fuse_config *cfg)
+{
+	printf("@@@@@@@INIT start @@@@@@@\n");
+}
+
+static const struct fuse_operations ot_oper = {
+	.init           = ot_init,
+	/*
+	.mkdir		= ot_mkdir,
+	.rmdir		= ot_rmdir,
+	.getattr	= ot_getattr,
+	.readdir	= ot_readdir,
+	.open		= ot_open,
+        .create		= ot_create,
+	.read		= ot_read,
+    	.write		= ot_write,
+        .unlink		= ot_unlink,
+        .utimens        = ot_utimens,
+	.release	= ot_release,
+	.rename		= ot_rename,	
+	.destroy	= ot_destroy,
+	*/
+};
+
+
+int main(int argc, char *argv[])
+{
+	int ret;
+	struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
+
+	/* Set defaults -- we have to use strdup so that
+	   fuse_opt_parse can free the defaults if other
+	   values are specified */
+	options.filename = strdup("hello");
+	options.contents = strdup("Hello World!\n");
+
+	/* Parse options */
+	if (fuse_opt_parse(&args, &options, option_spec, NULL) == -1)
+		return 1;
+
+	/* When --help is specified, first print our own file-system
+	   specific help text, then signal fuse_main to show
+	   additional help (by adding `--help` to the options again)
+	   without usage: line (by setting argv[0] to the empty
+	   string) */
+	if (options.show_help) {
+		show_help(argv[0]);
+		assert(fuse_opt_add_arg(&args, "--help") == 0);
+		args.argv[0][0] = '\0';
+	}
+
+	ret = fuse_main(args.argc, args.argv, &ot_oper, NULL);
+	fuse_opt_free_args(&args);
+	return ret;
+}
